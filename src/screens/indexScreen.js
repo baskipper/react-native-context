5957558cc24099295e8c1d165d02c1922c428bc6
bskipper <baskipper67@gmail.com>
2019-09-25 10:17:32 -0500

    Passing down info from context

---
+++ src/screens/indexScreen.js
@@ -0,0 +1,28 @@
+import React, {useContext} from 'react';
+import {View, Text, StyleSheet, FlatList} from "react-native";
+import BlogContext from "../context/BlogContext";
+
+const IndexScreen = () => {
+    const blogPosts = useContext(BlogContext);
+    return (
+        <View>
+            <Text>
+                Index Screen
+            </Text>
+            <FlatList
+            data={blogPosts}
+            keyExtractor={blogPost => blogPost.title}
+            renderItem={({item})=>{
+                return (
+                    <Text>
+                        {item.title}
+                    </Text>
+                )
+            }}
+            />
+
+        </View>
+    )
+}
+
+export default IndexScreen
