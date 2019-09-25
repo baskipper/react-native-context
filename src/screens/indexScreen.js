1340722b5e22477915fb14c0c14f21991867972e
bskipper <baskipper67@gmail.com>
2019-09-25 15:33:53 -0500

    Made createDataContext

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,28 +1,47 @@
 import React, {useContext} from 'react';
-import {View, Text, StyleSheet, FlatList} from "react-native";
-import BlogContext from "../context/BlogContext";
+import {View, Text, StyleSheet, FlatList, Button} from "react-native";
+import {Context as BlogContext} from "../context/BlogContext";
+import {Feather} from '@expo/vector-icons'
 
 const IndexScreen = () => {
-    const blogPosts = useContext(BlogContext);
+    const {state, addBlogPost} = useContext(BlogContext);
     return (
         <View>
-            <Text>
-                Index Screen
-            </Text>
+            <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList
-            data={blogPosts}
-            keyExtractor={blogPost => blogPost.title}
-            renderItem={({item})=>{
-                return (
-                    <Text>
-                        {item.title}
-                    </Text>
-                )
-            }}
+                data={state}
+                keyExtractor={blogPost => blogPost.title}
+                renderItem={({item}) => {
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>
+                                {item.title}
+                            </Text>
+                            <Feather name='trash' style={styles.icon}/>
+                        </View>
+                    )
+                }}
             />
 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+})
 
 export default IndexScreen
